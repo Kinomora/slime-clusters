474a8c71d316b341c1851ff6b66af7d00e7cb0da
Kinomora <christmasallthetime@gmail.com>
2020-10-29 01:40:04 -0400

    added cl_args support

---
+++ src/main/java/com/rayferric/slimeclusters/Main.java
@@ -0,0 +1,72 @@
+package com.rayferric.slimeclusters;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+//
+// CUDA notes:
+//
+// 65536x65536 region takes 512 MiB of memory.
+//
+// Doing this with temporary per-cluster caches runs about 3 times slower.
+//
+// Testing one seed in this region on a single 2.66 GHz core takes up to 3 minutes and we have
+// a whole lot of them to test, so maybe we can settle on a 512x512 region (32 kiB)?
+//
+// This will also fit nicely in the standard 48 kiB of per-thread GPU memory.
+//
+// The execution of a single thread should take about 0.01 s on 2.66 GHz in this scenario.
+//
+// Assuming that a single CUDA thread is about 10 times slower than that,
+// running this routine can yield a maximum of 10240 seeds/s per-block performance.
+//
+
+public class Main {
+    public static void main(@NotNull String[] args) throws IOException {
+        //Arg variables
+        int minimumClusterSize, chunkRange;
+
+        if(args.length == 0){
+            minimumClusterSize = Integer.parseInt("23");
+            chunkRange = Integer.parseInt("1024");
+        } else if (args.length == 2) {
+            minimumClusterSize = Integer.parseInt(args[0]);
+            chunkRange = Integer.parseInt(args[1]);
+        } else {
+            minimumClusterSize = Integer.parseInt("23");
+            chunkRange = Integer.parseInt("1024");
+        }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("clusters.txt"));
+
+        // Maps world seeds to lists of clusters:
+        SearchMapper mapper = new SearchMapper(chunkRange, minimumClusterSize);
+
+        // Get a stream of seeds to test:
+        LongStream seeds = LongStream.range(0L, 1L << 48);
+
+        // Map them to cluster lists and flatten into a single stream:
+        Stream<Cluster> clusters = seeds.mapToObj(mapper::run).flatMap(List::stream).parallel();
+
+        // Process and print:
+        clusters.forEach(cluster -> {
+            try {
+                System.out.println(cluster);
+                writer.write(cluster.toString());
+                writer.newLine();
+                writer.flush();
+            } catch(IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        });
+
+        writer.close();
+    }
+}
---
+++ src/main/java/com/rayferric/slimeclusters/SearchMapper.java
@@ -0,0 +1,88 @@
+package com.rayferric.slimeclusters;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Maps world seeds to lists of clusters.
+ */
+public class SearchMapper {
+    /**
+     * Constructs new cluster search mapper.
+     *
+     * @param extent  the extents of the searched region in chunks
+     * @param minSize minimum amount of chunks in a cluster
+     */
+    public SearchMapper(int extent, int minSize) {
+        this.extent = extent;
+        this.minSize = minSize;
+    }
+
+    /**
+     * Maps single world seed to a list of clusters.
+     *
+     * @param worldSeed world seed
+     *
+     * @return list of clusters
+     */
+    public List<Cluster> run(long worldSeed) {
+        List<Cluster> clusters = new ArrayList<>();
+
+        BitField cache = new BitField((extent * 2L) * (extent * 2L));
+
+        for(int chunkX = -extent; chunkX < extent; chunkX++) {
+            for(int chunkZ = -extent; chunkZ < extent; chunkZ++) {
+                int size = findClusters(cache, worldSeed, chunkX, chunkZ);
+                if(size >= minSize)
+                    clusters.add(new Cluster(worldSeed, chunkX, chunkZ, size));
+            }
+        }
+
+        return clusters;
+    }
+
+    private final int extent, minSize;
+
+    private int findClusters(@NotNull BitField cache, long worldSeed, int chunkX, int chunkZ) {
+        // Map two-dimensional position to a one-dimensional index:
+        long cacheIdx = (long)(chunkX + extent) * (extent * 2) + (chunkZ + extent);
+
+        // We don't want to process chunks multiple times:
+        if(cache.get(cacheIdx))
+            return 0;
+
+        cache.set(cacheIdx, true);
+
+        if(!checkSlimeChunk(worldSeed, chunkX, chunkZ))
+            return 0;
+
+        int count = 1;
+
+        // Process neighboring chunks:
+        if(chunkX + 1 < extent)
+            count += findClusters(cache, worldSeed, chunkX + 1, chunkZ);
+        if(chunkX - 1 >= -extent)
+            count += findClusters(cache, worldSeed, chunkX - 1, chunkZ);
+        if(chunkZ + 1 < extent)
+            count += findClusters(cache, worldSeed, chunkX, chunkZ + 1);
+        if(chunkZ - 1 >= -extent)
+            count += findClusters(cache, worldSeed, chunkX, chunkZ - 1);
+
+        return count;
+    }
+
+    // Name is self-explanatory. Checks whether a given chunk has the ability to spawn slimes.
+    private boolean checkSlimeChunk(long worldSeed, int chunkX, int chunkZ) {
+        worldSeed += chunkX * chunkX * 0x4C1906;
+        worldSeed += chunkX * 0x5AC0DB;
+        worldSeed += chunkZ * chunkZ * 0x4307A7L;
+        worldSeed += chunkZ * 0x5F24F;
+        worldSeed ^= 0x3AD8025FL;
+
+        Random rand = new Random(worldSeed);
+        return rand.nextInt(10) == 0;
+    }
+}
